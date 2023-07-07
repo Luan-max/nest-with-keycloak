9fdead086df7d9eb05fc78acf8781b718550317b
Luan-max <luannlopessilva@outlook.com>
2023-07-07 13:42:09 -0300

    feat: fix env

--- nest-multi-tenancy-keycloak/src/infra/auth/auth/auth.service.ts
+++ nest-multi-tenancy-keycloak/src/infra/auth/auth/auth.service.ts
@@ -1,11 +1,16 @@
 import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { firstValueFrom } from 'rxjs';
 import { PrismaService } from 'src/infra/database/prisma/prisma.service';
 
 @Injectable()
 export class AuthService {
-  constructor(private http: HttpService, private prisma: PrismaService) {}
+  constructor(
+    private http: HttpService,
+    private prisma: PrismaService,
+    private configService: ConfigService,
+  ) {}
 
   async login(username: string, password: string) {
     const user = await this.prisma.user.findFirst({
@@ -23,13 +28,15 @@ export class AuthService {
     const { data } = await firstValueFrom(
       this.http.post(
         `http://localhost:8080/auth/realms/${
-          tenant ? tenant.subdomain.toLocaleLowerCase() : 'studing'
+          tenant
+            ? tenant.subdomain.toLocaleLowerCase()
+            : this.configService.get('TENANT_MASTER')
         }/protocol/openid-connect/token`,
         new URLSearchParams({
           client_id: 'nest',
           client_secret: tenant
             ? tenant.secretKey
-            : '702bcd77-1c3f-47fe-b1a0-065666dc04d7',
+            : this.configService.get('SECRET_KEY'),
           grant_type: 'password',
           username,
           password,
